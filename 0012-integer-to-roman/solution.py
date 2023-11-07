3d702a6f3d8e7e04621c4e0245ba94bc1f829ac9
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2023-11-07 16:05:09 +0000

    [LeetCode Sync] Runtime - 47 ms (5.27%), Memory - 16.4 MB (99.96%)

---
+++ 0012-integer-to-roman/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    
+    VALUES = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    ROMAN_LETTERS =["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    
+    def intToRoman(self, num):
+        if type(num) != int:
+            raise TypeError('argument must be of type int')
+        if (num < 1 or num > 3999):
+            raise ValueError('argument must be in range [1-3999]')
+        roman = []
+        for value, roman_letter in zip(self.VALUES, self.ROMAN_LETTERS):
+            while(num >= value):
+                num = num - value
+                roman.append(roman_letter)
+        return ''.join(roman)
