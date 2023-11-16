ce2e2389facd867afe1a09f537400f695ff31815
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2023-11-16 16:26:02 +0000

    [LeetCode Sync] Runtime - 5 ms (0.38%), Memory - 6.7 MB (100.00%)

---
+++ 0027-remove-element/solution.c
@@ -0,0 +1,10 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int count = 0;
+    for(int i=0;i<numsSize;i++){
+        if(*(nums+i) != val){
+            *(nums+count) = *(nums+i);
+            count = count + 1;
+        }
+    }
+    return count;
+}
