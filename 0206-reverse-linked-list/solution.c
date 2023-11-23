fa19578ee01d42a10a2ce575b1c25e9e5fca0533
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2023-11-23 17:34:56 +0000

    [LeetCode Sync] Runtime - 2 ms (3.10%), Memory - 7.3 MB (100.00%)

---
+++ 0206-reverse-linked-list/solution.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+
+       //step 1
+        struct ListNode* prev_p = NULL;
+        struct ListNode* current_p = head;
+        struct ListNode* next_p;
+       
+       //step 2
+        while(current_p) {
+
+            next_p = current_p->next;
+            current_p->next = prev_p;
+            
+            prev_p = current_p;
+            current_p = next_p;
+        }
+
+        head = prev_p; //step 3
+        return head;
+    
+}
