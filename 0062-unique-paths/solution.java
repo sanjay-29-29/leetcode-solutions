a72497d081a1f781d27424b261c601a4f148a040
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-10-12 15:36:41 +0000

    [LeetCode Sync] Runtime - 8 ms (1.96%), Memory - 42.1 MB (8.01%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,25 +1,25 @@
 class Solution {
+    int row, col;
     HashMap<String, Integer> map;
     
     public int uniquePaths(int m, int n) {
         map = new HashMap<>();
-        return recursion(m,n);
+        return recursion(m, n);
     }
-
-    public int recursion(int m, int n){
-        String key = m + "," + n;
+    
+    private int recursion(int i, int j){
+        String key = i + "," + j;
         if(map.containsKey(key)){
             return map.get(key);
         }
-        if(m==1 && n == 1){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(i==1 && j==1){
             return 1;
         }
-        if(m==0 || n == 0){
-            return 0;
-        }else{
-            int val = recursion(m-1, n) + recursion(m, n-1);
-            map.put(key,val);
-            return val;
-        }
+        int val = recursion(i-1,j) + recursion(i,j-1);
+        map.put(key,val);
+        return val;
     }
 }
