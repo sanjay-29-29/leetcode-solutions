bdddf5f618607830efeea43893e88e7f91a19ef8
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-10-22 12:10:14 +0000

    [LeetCode Sync] Runtime - 529 ms (11.04%), Memory - 55.3 MB (5.32%)

--- 2470-removing-stars-from-a-string/solution.java
+++ 2470-removing-stars-from-a-string/solution.java
@@ -1,27 +1,19 @@
 class Solution {
     public String removeStars(String s) {
         Stack<String> stack = new Stack<>();
-        int index = 0;
 
-        while(index<s.length()){
-            //System.out.println(s.charAt(index));
-            if(s.charAt(index) == '*' && !stack.isEmpty()){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i)=='*'){
                 stack.pop();
-                index++;
             }else{
-                stack.push(String.valueOf(s.charAt(index)));
-                index++;
+                stack.push(String.valueOf(s.charAt(i)));
             }
         }
-        String str = "";
-        while(!stack.isEmpty()){
-            str += stack.pop();
+        String val = "";
+        List<String> s_arr = new ArrayList(stack);
+        for(int i=0;i<s_arr.size();i++ ){
+            val += s_arr.get(i);
         }
-        System.out.println(str);
-        String ret = "";
-        for(int i=str.length()-1;i>=0;i--){
-            ret += String.valueOf(str.charAt(i));
-        }
-        return ret;
+        return val;
     }
 }
