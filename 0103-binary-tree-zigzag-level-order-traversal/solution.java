694258f797df55b149e723a6fd46363bb0b1444f
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-10-26 16:33:32 +0000

    [LeetCode Sync] Runtime - 2 ms (2.65%), Memory - 42.3 MB (45.72%)

---
+++ 0103-binary-tree-zigzag-level-order-traversal/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        class Node{
+            TreeNode element;
+            int level;
+            Node(TreeNode element, int level){
+                this.element = element;
+                this.level = level;
+            }
+        }
+        
+        List<List<Integer>> returnList = new ArrayList<List<Integer>>();
+        HashSet<Integer> set = new HashSet<>();
+        Stack<Node> queue = new Stack<>();
+        List<Integer> list = new ArrayList<Integer>();
+        
+        queue.add(new Node(root,0));
+
+        while(!queue.isEmpty()){
+            Node node = queue.pop();
+            if(node.element == null) continue;
+            if(set.contains(node.level)){
+                List<Integer> l = returnList.get(node.level);
+                l.add(node.element.val);
+            }else{
+                List<Integer> ls = new ArrayList<Integer>();
+                ls.add(node.element.val);
+                returnList.add(ls);
+                set.add(node.level);
+            }
+            queue.push(new Node(node.element.right, node.level+1));
+            queue.push(new Node(node.element.left, node.level+1));
+        }
+
+        for(int i = 0; i < returnList.size(); i++){
+            if(i % 2 != 0){
+                List<Integer> l = returnList.get(i);
+                Collections.reverse(l);
+            }
+        }
+        return returnList;
+    }
+}
