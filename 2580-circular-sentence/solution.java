69cd8a7f795e34f64f3f42d07b201fe2a66c1e65
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-02 09:51:30 +0000

    [LeetCode Sync] Runtime - 1 ms (88.75%), Memory - 42 MB (53.56%)

---
+++ 2580-circular-sentence/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        if(sentence.charAt(0) != sentence.charAt(sentence.length() - 1)){
+            return false;
+        }
+        for(int i = 0; i < sentence.length(); i++){
+            if(sentence.charAt(i) == ' ' && 
+                sentence.charAt(i - 1) != sentence.charAt(i + 1)){
+                    return false;
+            }
+        }
+        return true;
+    }
+}
