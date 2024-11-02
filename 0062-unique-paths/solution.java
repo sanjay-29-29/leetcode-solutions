0751dec0f04eead6cf3e462270f7657fb5f5c6d4
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-02 13:56:30 +0000

    [LeetCode Sync] Runtime - 1 ms (8.86%), Memory - 40.1 MB (86.84%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,33 +1,15 @@
 class Solution {
-    int m, n;
-    HashMap<String, Integer> map = new HashMap<>();
-
     public int uniquePaths(int m, int n) {
-        this.m = m;
-        this.n = n;
-
-        return bfs(0, 0);
-    }
-
-    public int bfs(int i, int j){
-        boolean exp1 = (0 <= i && i < m);
-        boolean exp2 = (0 <= j && j < n);
-        String key = i + " " + j;
-
-        if(!exp1 || !exp2){
-            return 0;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][1] = 1;
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(j != n)
+                    dp[i][j + 1] += dp[i][j];
+                if(i != m)
+                    dp[i + 1][j] += dp[i][j];
+            }
         }
-
-        if(i == m - 1 && j == n - 1){
-            return 1;
-        }
-
-        if(map.containsKey(key)){
-            return map.get(key);
-        }
-        
-        int val =  bfs(i + 1, j) + bfs(i, j + 1);
-        map.put(key, val);
-        return val;
+        return dp[m][n];
     }
 }
