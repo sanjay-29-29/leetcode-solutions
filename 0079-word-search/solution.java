eb873d7804776cfdd2726b3d3985162c10c6332a
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-04 18:00:26 +0000

    [LeetCode Sync] Runtime - 168 ms (34.76%), Memory - 42.2 MB (24.64%)

--- 0079-word-search/solution.java
+++ 0079-word-search/solution.java
@@ -1,52 +1,50 @@
 class Solution {
     char[][] board;
     String word;
-    int[][] visitedArr;
+    int[][] visited;
 
     public boolean exist(char[][] board, String word) {
-        this.board = board;
+        this.board = board;    
         this.word = word;
-        visitedArr = new int[board.length][board[0].length];
-        boolean returnValue = false;
+        this.visited = new int[board.length][board[0].length];
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    returnValue = returnValue || isExist(i, j, 0);
-                    if(returnValue){
-                        return true;
-                    }
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(traverse(i, j, 0)){
+                    return true;
                 }
             }
         }
-        return returnValue;
+        return false;
     }
 
-    private boolean isExist(int i, int j, int index) {
+    private boolean traverse(int i, int j, int index){
         boolean exp1 = (0 <= i && i < board.length);
         boolean exp2 = (0 <= j && j < board[0].length);
 
-        if (!exp1 || !exp2) {
-            return false;
-        }
-
-        if (visitedArr[i][j] == 1) {
-            return false;
-        }
-
-        if (word.charAt(index) == board[i][j]) {
-            visitedArr[i][j] = 1;
-            index++;
-            if (index == word.length()) {
+        if(index == word.length()){
                 return true;
-            }
-            boolean val = isExist(i + 1, j, index) || isExist(i - 1, j, index)
-                    || isExist(i, j + 1, index) || isExist(i, j - 1, index);
-            if (val == true)
-                return val;
-            else
-                visitedArr[i][j] = 0;
         }
+
+        if(!exp1 || !exp2){
+            return false;
+        }
+
+        if(visited[i][j] == 1){
+            return false;
+        }
+
+        visited[i][j] = 1;
+
+        if(word.charAt(index) == board[i][j]){
+            boolean ret = traverse(i + 1, j, index + 1) ||
+                traverse(i - 1, j, index + 1) ||
+                traverse(i, j + 1, index + 1) ||
+                traverse(i, j - 1, index + 1);
+                visited[i][j] = 0;
+            return ret;
+        }
+        visited[i][j] = 0;
         return false;
     }
 }
