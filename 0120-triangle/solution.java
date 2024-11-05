2efd3d7c80737557800e9acbc73966ded1c2ec10
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-05 11:53:45 +0000

    [LeetCode Sync] Runtime - 15 ms (5.86%), Memory - 45 MB (13.60%)

--- 0120-triangle/solution.java
+++ 0120-triangle/solution.java
@@ -1,23 +1,28 @@
 class Solution {
-    HashMap<String, Integer> map;
+    List<List<Integer>> triangle;
+    Map<String, Integer> map = new HashMap<>();
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        map = new HashMap<>();
-        return minimumTotal(triangle, 0, 0);
+        this.triangle = triangle;
+        return recursion(0,0);
     }
 
-    private int minimumTotal(List<List<Integer>> triangle, int col, int row){
-        String key = col + "," + row;
+    private int recursion(int list, int index){
+        if(list > triangle.size() - 1){
+            return 0;
+        }
+        if(index > triangle.get(list).size()){
+            return Integer.MAX_VALUE;
+        }
+        
+        String key = list + " " + index;
+
         if(map.containsKey(key)){
             return map.get(key);
         }
-        if(col > triangle.size() - 1){
-            return 0;
-        }
-
-        int currentVal = triangle.get(col).get(row);
-        int val = Math.min(currentVal + minimumTotal(triangle, col + 1, row), currentVal + minimumTotal(triangle, col + 1, row+1));
+        
+        int val = triangle.get(list).get(index) + Math.min(recursion(list + 1, index), recursion(list + 1, index + 1));
         map.put(key, val);
         return val;
-    }
+    }   
 }
