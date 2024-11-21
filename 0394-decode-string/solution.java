bed9b3dd640aa2b5916d83162cd9caa7df3d2f53
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-21 16:16:49 +0000

    [LeetCode Sync] Runtime - 6 ms (5.25%), Memory - 41.9 MB (17.16%)

--- 0394-decode-string/solution.java
+++ 0394-decode-string/solution.java
@@ -1,90 +1,45 @@
-// class Solution {
-//     public String decodeString(String s) {
-//         int i = 0;
-//         int n = 0;
-//         while (i < s.length()) {
-//             char c = s.charAt(i);
-
-//             if (c == '[') {
-//                 int count = 1;
-//                 int j = 0;
-//                 String characters = "";
-
-//                 while (count > 0) {
-//                     j++;
-//                     if (s.charAt(i + j) == '[')
-//                         count++;
-//                     if (s.charAt(i + j) == ']')
-//                         if (count - 1 == 0) {
-//                             break;
-//                         } else {
-//                             count--;
-//                         }
-//                     characters += s.charAt(i + j);
-//                 }
-//                 System.out.println(characters);
-//                 for(int i = 0; i < n; i++){
-
-//                 }
-//                 i = i + j;
-//             } else {
-//                 n += (c - '0') * 10;
-//                 System.out.println(n);
-//             }
-//             i++;
-//         }
-
-//         return s;
-//     }
-// }
 class Solution {
     public String decodeString(String s) {
-        return recursion(s);
+        return decode(s);
     }
 
-    private String recursion(String s) {
-        int n = 0;
-        String newStr = "";
+    private String decode(String s) {
         int i = 0;
+        int n = 0;
+        String retString = "";
 
-        while(i < s.length()) {
-            char c = s.charAt(i);
+        while (i < s.length()) {
             if (s.charAt(i) == '[') {
-                int j = 0;
-                int count = 1;
-                String characters = "";
+                int j = i + 1;
+                int inCount = 1;
 
-                while (count > 0) {
+                String newStr = "";
+
+                while (true) {
+                    if (s.charAt(j) == ']')
+                        inCount--;
+                    if (s.charAt(j) == '[')
+                        inCount++;
+                    if(inCount == 0) break;
+                    newStr += s.charAt(j);
                     j++;
-                    if (s.charAt(i + j) == '[')
-                        count++;
-                    if (s.charAt(i + j) == ']')
-                        if (count - 1 == 0) {
-                            break;
-                        } else {
-                            count--;
-                        }
-                    characters += s.charAt(i + j);
                 }
 
-                String recursionRes = recursion(characters);
-                
-                for (int k = 0; k < n; k++) {
-                    newStr +=  recursionRes;
-                }
+                String funcStr = decode(newStr);
+                for (int k = 0; k < n; k++) retString += funcStr;
 
+                i = j;
                 n = 0;
-                i = i + j;
-            }
-            else if( !(c >= '0') || !(c <= '9')){
-                newStr += c;
+
+            } else if (s.charAt(i) >= 97 && s.charAt(i) <= 122) {
+                retString += s.charAt(i);
                 n = 0;
-            }else{
-                n = n * 10 + (c - '0');
+            } else {
+                n = n * 10 + (s.charAt(i) - '0');
             }
             i++;
         }
 
-        return newStr;
+        return retString;
     }
 }
