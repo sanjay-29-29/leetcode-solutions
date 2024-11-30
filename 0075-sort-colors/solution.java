d9fceea01ed1a3e97d3df5055d76514e60e86806
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-30 09:27:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (68.04%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int hi = nums.length - 1;
+        int low = 0;
+        int mid = 0;
+
+        while(mid <= hi){
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                int temp = nums[mid];
+                nums[mid] = nums[hi];
+                nums[hi] = temp;
+                hi--;
+            }
+            else{
+                mid++;
+            }
+        }
     }
 }
