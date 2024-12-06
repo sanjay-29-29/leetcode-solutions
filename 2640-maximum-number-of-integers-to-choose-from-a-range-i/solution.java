f565d0a04d35e2c3ccb32984eae1f95fbe00691f
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-12-06 04:31:23 +0000

    [LeetCode Sync] Runtime - 51 ms (29.79%), Memory - 46.6 MB (7.60%)

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < banned.length; i++){
+            set.add(banned[i]);
+        }
+
+        int sum = 0;
+        int count = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(!set.contains(i) && sum + i <= maxSum){
+                sum += i;
+                count ++;
+            }
+        }
+
+        return count;
+    }
+}
