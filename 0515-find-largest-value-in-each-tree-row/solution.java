7e25962db50e151c6917c52bcd93cd0c27c87016
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-12-25 16:19:07 +0000

    [LeetCode Sync] Runtime - 5 ms (3.61%), Memory - 45.8 MB (10.75%)

--- 0515-find-largest-value-in-each-tree-row/solution.java
+++ 0515-find-largest-value-in-each-tree-row/solution.java
@@ -1,56 +1,62 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    class Node {
-        TreeNode tree;
+    class Node{
+        TreeNode node;
         int level;
 
-        Node(TreeNode _tree, int _level) {
-            tree = _tree;
+        Node(TreeNode _node, int _level){
+            node = _node;
             level = _level;
         }
     }
 
     public List<Integer> largestValues(TreeNode root) {
-        Queue<Node> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-
+        Queue<Node> queue = new LinkedList<>();     
+        List<Integer> list = new ArrayList<>(); 
         queue.add(new Node(root, 0));
-        int currLevel = 0;
 
-        while (!queue.isEmpty()) {
-            Node node = queue.remove();
+        int level = -1;
+        int maxVal = 0;
 
-            if (node.tree == null) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+
+        while(!queue.isEmpty()){
+            Node curr = queue.poll();
+            if(curr.node == null){
                 continue;
             }
 
-            if (!set.contains(node.level)) {
-                list.add(node.tree.val);
-                set.add(node.level);
+            if(curr.level != level){
+                if(level != -1){
+                    list.add(maxVal);
+                }
+                level = curr.level;
+                maxVal = curr.node.val; 
             }
 
-            if(list.get(node.level) < node.tree.val){
-                list.set(node.level, node.tree.val);
-            }
+            maxVal = Math.max(curr.node.val, maxVal);
 
-            queue.add(new Node(node.tree.left, node.level + 1));
-            queue.add(new Node(node.tree.right, node.level + 1));
+            queue.add(new Node(curr.node.left, curr.level + 1));
+            queue.add(new Node(curr.node.right, curr.level + 1));
         }
+
+        list.add(maxVal);
 
         return list;
     }
