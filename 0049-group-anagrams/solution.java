cf1ba9c5ed932040d457e724a796ab4f23c2227f
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-12-28 14:01:51 +0000

    [LeetCode Sync] Runtime - 59 ms (5.22%), Memory - 52 MB (5.42%)

--- 0049-group-anagrams/solution.java
+++ 0049-group-anagrams/solution.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Map<Character, Integer>, Integer> hash = new HashMap<>();
-        List<List<String>> list = new ArrayList<>();
-        
-        for(int i = 0; i < strs.length; i++){
+        Map<Map<Character, Integer>, List<String>> Hmap = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
             Map<Character, Integer> map = new HashMap<>();
-            for(int j = 0; j < strs[i].length(); j++){
-                if(map.containsKey(strs[i].charAt(j))){
+
+            for (int j = 0; j < strs[i].length(); j++) {
+                if (map.containsKey(strs[i].charAt(j))) {
                     map.put(strs[i].charAt(j), map.get(strs[i].charAt(j)) + 1);
-                }else{
+                } else {
                     map.put(strs[i].charAt(j), 1);
                 }
             }
-            if(hash.containsKey(map)){
-                int listIndex = hash.get(map);
-                List<String> strList = list.get(listIndex);
-                strList.add(strs[i]);
-            }else{
-                hash.put(map, list.size());
-                List<String> newList = new ArrayList<>();
-                newList.add(strs[i]);
-                list.add(newList);
+
+            if (Hmap.containsKey(map)) {
+                List<String> list = Hmap.get(map);
+                list.add(strs[i]);
+            } else {
+                List<String> list = new ArrayList<>(); 
+                list.add(strs[i]);
+                Hmap.put(map, list);
             }
+        }
+
+        List<List<String>> list = new ArrayList<>();
+        for (List<String> l : Hmap.values()) {
+            list.add(l);
         }
 
         return list;
