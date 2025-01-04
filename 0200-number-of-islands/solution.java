b54cc64318f151929c7c042c8c5c2ad1e9198ba3
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-04 13:18:52 +0000

    [LeetCode Sync] Runtime - 12 ms (8.54%), Memory - 48.9 MB (93.39%)

--- 0200-number-of-islands/solution.java
+++ 0200-number-of-islands/solution.java
@@ -1,41 +1,71 @@
 class Solution {
-    HashSet<String> set;
-    public int numIslands(char[][] grid) {
-        set = new HashSet<>();
-        int count = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(traverse(grid, i, j) == true){
-                    count++;
-                }
-            }
+    class Node {
+        int i, j;
+
+        Node(int _i, int _j) {
+            i = _i;
+            j = _j;
         }
-        return count;
     }
 
-    private boolean traverse(char[][] grid, int i, int j){
-        boolean exp1 = (0 <= i && i<grid.length);
-        boolean exp2 = (0 <= j && j<grid[0].length);
-        if(!exp1 || !exp2) return false;
+    public int numIslands(char[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int noOfIslands = 0;
 
-        String pos = i + "," + j;
-
-        if(set.contains(pos)){
-            return false;
-        };
-
-        if(grid[i][j] == '0'){
-            return false;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                noOfIslands += bfs(i, j, grid, visited);
+            }
         }
 
-        set.add(pos);
-        
-        traverse(grid, i + 1 , j);
-        traverse(grid, i , j + 1);
-        traverse(grid, i - 1, j);
-        traverse(grid, i , j - 1);
-        
-        System.out.println(pos);
-        return true;
+        return noOfIslands;
+    }
+
+    private boolean isOutOfBound(int i, int j, int row, int col) {
+        boolean a = (0 <= i && i < row);
+        boolean b = (0 <= j && j < col);
+
+        if (!a || !b) {
+            return true;
+        }
+        return false;
+    }
+
+    private int bfs(int i, int j, char[][] grid, boolean[][] visited) {
+        if (visited[i][j] || grid[i][j] == '0') {
+            return 0;
+        }
+
+        int[][] directions = {
+                { 0, 1 },
+                { 1, 0 },
+                { 0, -1 },
+                { -1, 0 }
+        };
+
+        int rowMax = visited.length;
+        int colMax = visited[0].length;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(i, j));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            if (isOutOfBound(node.i, node.j, rowMax, colMax) || visited[node.i][node.j]
+                    || grid[node.i][node.j] == '0') {
+                continue;
+            }
+
+            visited[node.i][node.j] = true;
+
+            for (int[] direction : directions) {
+                int newX = node.i + direction[0];
+                int newY = node.j + direction[1];
+                queue.add(new Node(newX, newY));
+            }
+        }
+
+        return 1;
     }
 }
