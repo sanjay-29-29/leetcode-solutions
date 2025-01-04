10a8f3a7ab22f98ab3db940a57d726e3b091c766
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-04 13:13:23 +0000

    [LeetCode Sync] Runtime - 6 ms (6.73%), Memory - 45.5 MB (5.32%)

--- 0695-max-area-of-island/solution.java
+++ 0695-max-area-of-island/solution.java
@@ -1,39 +1,72 @@
 class Solution {
-    int maxArea;
-    HashSet<String> set;
+    class Node{
+        int i, j;
+        Node(int _i,int _j){
+            i = _i;
+            j = _j;
+        }
+    }
 
     public int maxAreaOfIsland(int[][] grid) {
-        set = new HashSet<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 1){
-                    maxArea = Math.max(maxArea, traverseGrid(grid, i, j));
-                }
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int max = 0;
+
+        for(int i = 0; i < grid.length; i++){
+           for(int j = 0; j < grid[0].length; j++) {
+                max = Math.max(max, bfs(i, j, grid, visited));
+           }
+        }
+
+        return max;
+    }
+
+    private boolean isOutOfBound(int i, int j, int row, int col){
+        boolean a = (0 <= i && i < row);
+        boolean b = (0 <= j && j < col);
+
+        if(!a || !b){
+            return true;
+        }
+        return false;
+    }
+
+    private int bfs(int i, int j, int[][] grid, boolean[][] visited){
+        if(visited[i][j]){
+            return 0;
+        }
+
+        int[][] directions = {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0}
+        };
+        
+        int rowMax = visited.length;
+        int colMax = visited[0].length;
+
+        Queue<Node> queue = new LinkedList<>();     
+        queue.add(new Node(i, j));
+
+        int area = 0;
+
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+
+            if(isOutOfBound(node.i, node.j, rowMax, colMax) || visited[node.i][node.j] || grid[node.i][node.j] == 0){
+                continue;
+            }
+
+            visited[node.i][node.j] = true;
+            area++;
+
+            for(int[] direction : directions){
+                int newX = node.i + direction[0];
+                int newY = node.j + direction[1];
+                queue.add(new Node(newX, newY));
             }
         }
-        return maxArea;
-    }
-    
-    private int traverseGrid(int grid[][], int i, int j){
-        boolean exp1 = (0 <= i && i < grid.length);
-        boolean exp2 = (0 <= j && j < grid[0].length);
-        if(!exp1 || !exp2) return 0;
-        if(grid[i][j] == 0){
-            return 0;
-        }
-        
-        String pos = i + "," + j;
-        if(set.contains(pos)){
-            return 0;
-        }
-        set.add(pos);
 
-        int currArea = 1;
-        currArea += traverseGrid(grid, i+1, j);
-        currArea += traverseGrid(grid, i-1, j); 
-        currArea += traverseGrid(grid, i, j+1); 
-        currArea += traverseGrid(grid, i, j-1);
-
-        return currArea;
+        return area;
     }
 }
