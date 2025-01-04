c142ba96c220b4e21ca1b543ac2ad6afead109af
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-04 09:32:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (79.64%)

--- 0112-path-sum/solution.java
+++ 0112-path-sum/solution.java
@@ -1,32 +1,28 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null){
+        if (root == null) {
             return false;
         }
-        return recursion(root, targetSum);
-    }
-    private boolean recursion(TreeNode root, int targetSum){
-        if(root != null && targetSum-root.val==0 && root.left == null && root.right == null){
+
+        if (root.left == null && root.right == null && targetSum - root.val == 0) {
             return true;
         }
-        if(root == null){
-            return false;
-        }
-        return recursion(root.left, targetSum-root.val) || recursion(root.right, targetSum-root.val);
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
