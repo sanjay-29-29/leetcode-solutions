dabfa5f9e41035332363b3e57d123ce093fd0609
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-04 09:17:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (35.68%)

--- 0129-sum-root-to-leaf-numbers/solution.java
+++ 0129-sum-root-to-leaf-numbers/solution.java
@@ -1,42 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    List<String> list;
+    // int val;
+    // Set<TreeNode> set = new HashSet<>();
+
     public int sumNumbers(TreeNode root) {
-        list = new ArrayList<String>();
-        recursion(root, "");
-        int sum = 0;
-        for(int i=0;i<list.size();i++){
-            sum += Integer.parseInt(list.get(i));
-        }
-        return sum;
-    }
-    private void recursion(TreeNode root, String current){
-        String newString;
-        if(root!=null && root.left == null && root.right == null){
-            newString = current + root.val;
-            list.add(newString);
-            return;
-        }
-        if(root == null){
-            return;
-        }
-        newString = current + root.val;
-        recursion(root.left, newString);
-        recursion(root.right, newString);
+        return traverse(root, 0);
+        // return val;
     }
 
+    private int traverse(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            // System.out.println(sum);
+            sum = (sum * 10) + root.val;
+            return sum;
+        }
+
+        // System.out.println(root.val);
+        sum = (sum * 10) + root.val;
+
+        return traverse(root.left, sum) +
+                traverse(root.right, sum);
+    }
 }
