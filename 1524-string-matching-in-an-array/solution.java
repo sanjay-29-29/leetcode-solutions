770dbe237079fef7a264ce5bf9d9b4590259c022
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-07 16:35:20 +0000

    [LeetCode Sync] Runtime - 5 ms (41.61%), Memory - 42.3 MB (37.89%)

---
+++ 1524-string-matching-in-an-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        Set<String> res = new HashSet<>();
+
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < words.length; j++){
+                if(i != j && words[i].contains(words[j])){
+                    res.add(words[j]);
+                }
+            }
+        }
+
+        return new ArrayList<>(res);
+    }
+}
