7b7e61d20a1b397af878c6c08db965f303f83ae3
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-08 12:47:20 +0000

    [LeetCode Sync] Runtime - 138 ms (70.13%), Memory - 42.1 MB (26.00%)

--- 0079-word-search/solution.java
+++ 0079-word-search/solution.java
@@ -1,50 +1,45 @@
 class Solution {
-    char[][] board;
-    String word;
-    int[][] visited;
-
-    public boolean exist(char[][] board, String word) {
-        this.board = board;    
-        this.word = word;
-        this.visited = new int[board.length][board[0].length];
-
+    public boolean exist(char[][] board, String word) { 
+        boolean[][] visited = new boolean[board.length][board[0].length];
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
-                if(traverse(i, j, 0)){
+                if(traverse(board, i, j, word, 0, visited)){
                     return true;
                 }
             }
         }
+
         return false;
     }
 
-    private boolean traverse(int i, int j, int index){
-        boolean exp1 = (0 <= i && i < board.length);
-        boolean exp2 = (0 <= j && j < board[0].length);
-
+    private boolean traverse(char[][] board, int i, int j, String word, int index, boolean[][] visited){
         if(index == word.length()){
-                return true;
+            return true;
         }
 
-        if(!exp1 || !exp2){
+        if(!(0 <= i && i < board.length) || !(0 <= j && j < board[0].length)){
             return false;
         }
 
-        if(visited[i][j] == 1){
+        if(visited[i][j]){
             return false;
         }
-
-        visited[i][j] = 1;
 
         if(word.charAt(index) == board[i][j]){
-            boolean ret = traverse(i + 1, j, index + 1) ||
-                traverse(i - 1, j, index + 1) ||
-                traverse(i, j + 1, index + 1) ||
-                traverse(i, j - 1, index + 1);
-                visited[i][j] = 0;
-            return ret;
+            visited[i][j] = true;
+            boolean res = traverse(
+                board, i + 1, j, word, index + 1, visited
+            ) || traverse(
+                board, i, j + 1, word, index + 1, visited
+            ) || traverse(
+                board, i - 1, j, word, index + 1, visited
+            ) || traverse(
+                board, i, j - 1, word, index + 1, visited
+            );
+            visited[i][j] = false;
+            return res;
         }
-        visited[i][j] = 0;
+
         return false;
     }
 }
