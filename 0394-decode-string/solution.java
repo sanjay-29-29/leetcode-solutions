2a59223e3f482953d8ca49980bb9fab07b742ffd
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-10 13:44:14 +0000

    [LeetCode Sync] Runtime - 5 ms (12.93%), Memory - 42.3 MB (6.20%)

--- 0394-decode-string/solution.java
+++ 0394-decode-string/solution.java
@@ -1,45 +1,50 @@
 class Solution {
     public String decodeString(String s) {
-        return decode(s);
-    }
-
-    private String decode(String s) {
+        String res = "";
         int i = 0;
-        int n = 0;
-        String retString = "";
+        int bracketCount = 0; 
+        int number = 0;
 
-        while (i < s.length()) {
-            if (s.charAt(i) == '[') {
+        while(i < s.length()){
+            if(s.charAt(i) == '['){
+                bracketCount++;         
+                String charInBracket = "";
                 int j = i + 1;
-                int inCount = 1;
-
-                String newStr = "";
-
-                while (true) {
-                    if (s.charAt(j) == ']')
-                        inCount--;
-                    if (s.charAt(j) == '[')
-                        inCount++;
-                    if(inCount == 0) break;
-                    newStr += s.charAt(j);
+                while(j < s.length()){
+                    if(s.charAt(j) == ']'){
+                        bracketCount--;
+                        if(bracketCount == 0){
+                            break;
+                        }
+                        charInBracket += s.charAt(j);
+                    }
+                    else if(s.charAt(j) == '['){
+                        charInBracket += s.charAt(j);
+                        bracketCount++;
+                    }
+                    else{
+                        charInBracket += s.charAt(j);
+                    }
                     j++;
                 }
-
-                String funcStr = decode(newStr);
-                for (int k = 0; k < n; k++) retString += funcStr;
-
                 i = j;
-                n = 0;
-
-            } else if (s.charAt(i) >= 97 && s.charAt(i) <= 122) {
-                retString += s.charAt(i);
-                n = 0;
-            } else {
-                n = n * 10 + (s.charAt(i) - '0');
+                String decoded = decodeString(charInBracket);
+                for(int k = 0; k < number; k++){
+                    res += decoded;
+                }
+                //System.out.println(res);
+                number = 0;
+            }
+            else if(s.charAt(i) >= 48 && s.charAt(i) <= 57){
+                number = number * 10;
+                number += s.charAt(i) - 48;
+            }
+            else{
+                res += s.charAt(i);
             }
             i++;
         }
 
-        return retString;
+        return res;
     }
 }
