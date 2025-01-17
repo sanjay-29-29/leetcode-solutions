8d6062a24646fab391816108c5c59dc0fd936339
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-17 13:52:23 +0000

    [LeetCode Sync] Runtime - 18 ms (5.20%), Memory - 45.2 MB (5.47%)

--- 0022-generate-parentheses/solution.java
+++ 0022-generate-parentheses/solution.java
@@ -1,30 +1,40 @@
 class Solution {
-    List<String> list = new ArrayList<String>();
+    public boolean validateParanthesis(String s){
+        Stack<Character> stack = new Stack<>();
 
-    public List<String> generateParenthesis(int n) {
-        gen(n * 2, "");
-        return list;
-    }
-
-    private void gen(int n, String s) {
-        if (n == 0) {
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '(') {
-                    stack.push('(');
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            }else{
+                if(stack.isEmpty()){
+                    return false;
                 }
-                if (s.charAt(i) == ')') {
-                    if (stack.isEmpty()) {
-                        return;
-                    }
-                    stack.pop();
+                char c = stack.pop();
+                if(c == ')'){
+                    return false;
                 }
             }
-            if (stack.isEmpty())
-                list.add(s);
+        }
+        if(stack.isEmpty())
+            return true;
+        return false;
+    }
+
+    private void generateParanth(String s, int n, List<String> res){
+        if(s.length() == n * 2){
+            if(validateParanthesis(s)){
+                res.add(s);
+            }
             return;
         }
-        gen(n - 1, s + ")");
-        gen(n - 1, s + "(");
+
+        generateParanth(s + ")", n, res);
+        generateParanth(s + "(", n, res);
+    }
+
+    public List<String> generateParenthesis(int n) {
+       List<String> res = new ArrayList<>();
+       generateParanth("", n, res);
+        return res;
     }
 }
