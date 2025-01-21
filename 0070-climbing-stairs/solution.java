78e7597aa33fe685530d2c768e2ce9cddd77dc07
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-21 09:10:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (27.25%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,21 +1,13 @@
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
-
     public int climbStairs(int n) {
-        if (n == 0) {
-            return 1;
-        }
+        int[] arr = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 1;
 
-        if (n < 0) {
-            return 0;
-        }
+        for(int i = 2; i < arr.length; i++){
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }    
 
-        if (map.containsKey(n)) {
-            return map.get(n);
-        }
-
-        int val = climbStairs(n - 1) + climbStairs(n - 2);
-        map.put(n, val);
-        return val;
+        return arr[n];
     }
 }
