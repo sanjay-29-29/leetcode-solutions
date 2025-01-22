f171a2ac25cc935a73dc1773efca71a87dc22acf
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-01-22 13:20:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41 MB (74.09%)

--- 0198-house-robber/solution.java
+++ 0198-house-robber/solution.java
@@ -1,22 +1,25 @@
 class Solution {
-    int[] nums;
-    Map<Integer, Integer> map = new HashMap<>();
-
     public int rob(int[] nums) {
-        this.nums = nums;
-        return robHouse(0);
-    }
-
-    private int robHouse(int index){
-        if(index >= nums.length){
-            return 0;
+        if(nums.length == 1){
+            return nums[0];
         }
-        if(map.containsKey(index)){
-            return map.get(index);
-        }    
 
-        int val = Math.max(nums[index] + robHouse(index + 2), robHouse(index + 1));
-        map.put(index, val);
-        return val;
+        int[] dp = new int[nums.length];
+        
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[nums.length - 1];
+        // return recursion(nums, 0);
     }
+    // private int recursion(int[] nums, int index){
+    //     if(index > nums.length - 1){
+    //         return 0;
+    //     }
+    //     return Math.max(recursion(nums, index + 1), nums[index] + recursion(nums, index + 2));
+    // }
 }
