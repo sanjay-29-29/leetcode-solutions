65cc76e7e3dde9ace910331cc6e5ca6306ea18ee
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-02-20 12:49:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (26.12%)

---
+++ 0081-search-in-rotated-sorted-array-ii/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target || nums[left] == target || nums[right] == target) {
+                return true;
+            }
+            if (nums[left] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
