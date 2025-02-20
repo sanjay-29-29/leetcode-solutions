8c84e3913d4bb5d89f67002fca6eceb723aacf11
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-02-20 15:23:52 +0000

    [LeetCode Sync] Runtime - 2 ms (88.01%), Memory - 56.7 MB (5.25%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -3,22 +3,21 @@ class Solution {
         int[] prefix = new int[nums.length];
         int[] suffix = new int[nums.length];
 
-        prefix[nums.length - 1] = 1;
-
-        for(int i = nums.length - 2; i >= 0; i--) {
-            prefix[i] = nums[i + 1] * prefix[i + 1];
+        Arrays.fill(prefix, 1);
+        Arrays.fill(suffix, 1);
+        
+        for(int i = nums.length - 2; i >= 0; i--){
+            prefix[i] = prefix[i + 1] * nums[i + 1];
         }
-
-        suffix[0] = 1;
 
         for(int i = 1; i < nums.length; i++){
             suffix[i] = suffix[i - 1] * nums[i - 1];
         }
 
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = prefix[i] * suffix[i];
+        for(int i = 0; i < prefix.length; i++){
+            prefix[i] = prefix[i] * suffix[i];
         }
 
-        return nums;
+        return prefix;
     }
 }
