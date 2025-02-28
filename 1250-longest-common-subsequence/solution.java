aad6754faed4679b85675985483157726d5a392b
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-02-28 15:09:09 +0000

    [LeetCode Sync] Runtime - 57 ms (5.05%), Memory - 51.3 MB (14.15%)

--- 1250-longest-common-subsequence/solution.java
+++ 1250-longest-common-subsequence/solution.java
@@ -1,40 +1,33 @@
 class Solution {
-    Map<String, Integer> map = new HashMap<>();
-    
     public int longestCommonSubsequence(String text1, String text2) {
-        return recursion(text1, text2, 0, 0);
+        int[][] dp = new int[text1.length()][text2.length()];
+
+        for(int i = 0; i < text1.length(); i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        return recursion(text1, text2, 0, 0, dp);
     }
-    
-    private int recursion(String text1, String text2, int index1, int index2){
-        if(index1 > text1.length() - 1 || index2 > text2.length() - 1){
+
+    private int recursion(String text1, String text2, int i1, int i2, int[][] dp){
+        if(i1 >= text1.length() || i2 >= text2.length()){
             return 0;
         }
 
-        String key = index1 + " " + index2;
-        int res = map.getOrDefault(key, - 1);
-        if(res != -1){
-            return res;
+        if(dp[i1][i2] != -1){
+            return dp[i1][i2];
         }
 
-        int val1 = 0;
-        if(text1.charAt(index1) == text2.charAt(index2)){
-            val1 = 1 + recursion(text1, text2, index1 + 1, index2 + 1);
+        if(text1.charAt(i1) == text2.charAt(i2)){
+            dp[i1][i2] = Math.max(1 + recursion(text1, text2, i1 + 1, i2 + 1, dp), Math.max(
+                recursion(text1, text2, i1, i2 + 1, dp),
+                recursion(text1, text2, i1 + 1, i2, dp)
+            ));
+            return dp[i1][i2];
         }
 
-        int val2 = recursion(
-            text1, text2, index1 + 1, index2
-        );
-        int val3 = recursion(
-            text1, text2, index1, index2 + 1
-        );
-        int max = Math.max(
-            val1,
-            Math.max(
-                val2, val3
-            )
-        );
-
-        map.put(key, max);
-        return max;
+        dp[i1][i2] = Math.max(recursion(text1, text2, i1, i2 + 1, dp),
+                recursion(text1, text2, i1 + 1, i2, dp));
+        return dp[i1][i2];
     }
 }
