951565d5a12c7fcaea8a84db36b099ff25f7faa1
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-02-28 15:41:34 +0000

    [LeetCode Sync] Runtime - 91 ms (88.71%), Memory - 49.1 MB (83.57%)

--- 0204-count-primes/solution.java
+++ 0204-count-primes/solution.java
@@ -1,25 +1,21 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n < 2) {
-            return 0;
-        }
+        boolean[] prime = new boolean[n];
 
-        int count = 0;
-        boolean[] arr = new boolean[n];
-
-        for (int i = 2; i < n; i++) {
-            if (arr[i]) {
-                continue;
-            }
-            count++;
-            if ((long)i * i < n) {
-                for (int j = i * i; j < n; j += i) {
-                    arr[j] = true;
+        for(int i = 2; i * i < n; i++){
+            if(!prime[i]){
+                for(int j = i * i; j < n; j += i){
+                    prime[j] = true;
                 }
             }
         }
 
+        int count = 0;
+        for(int i = 2; i < prime.length; i++){
+            if(!prime[i]){
+                count++;
+            }
+        }
         return count;
     }
 }
-
