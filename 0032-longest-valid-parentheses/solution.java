6eb3fc0858dd3be9d291bf7015c7e504dca211d8
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-03-24 09:50:56 +0000

    [LeetCode Sync] Runtime - 3 ms (74.47%), Memory - 44.9 MB (6.85%)

--- 0032-longest-valid-parentheses/solution.java
+++ 0032-longest-valid-parentheses/solution.java
@@ -1,51 +1,50 @@
 class Solution {
-    class Helper{
+    class Helper {
         char c;
-        int idx;
+        int index;
 
-        Helper(char _c, int _idx){
+        Helper(char _c, int _index) {
             c = _c;
-            idx = _idx;
+            index = _index;
         }
     }
 
     public int longestValidParentheses(String s) {
         int[] arr = new int[s.length()];
+
         Stack<Helper> stack = new Stack<>();
-
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(stack.isEmpty()){
-                stack.push(new Helper(c, i));
-            }
-            else if(c == '('){
-                stack.push(new Helper(c, i));
-            }
-            else{
-                Helper curr = stack.pop();
-                if(curr.c == '('){
-                    arr[curr.idx] = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (!stack.isEmpty()) {
+                    Helper h = stack.pop();
+                    arr[h.index] = 1;
                     arr[i] = 1;
-                }         
+                }
+            } else {
+                stack.push(new Helper('(', i));
             }
         }
 
-        int max = 0;
-        int curr = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.print(arr[i] + " ");
+        // }
 
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] == 1 && arr[i + 1] == 1){
-                curr++;
-                max = Math.max(curr, max);
-            }else{
-                curr = 0;
+        int left = 0, right = 0, max = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 1){
+                int j = i;
+                while(j < arr.length && arr[j] == 1){
+                    j++;
+                }
+                max = Math.max(j - i + 1, max);
+                i = j;
             }
         }
-        
-        if(max != 0){    
-            return max + 1;
-        }
 
-        return max;
+        if (max == 0) {
+            return max;
+        }
+        return max - 1;
     }
 }
