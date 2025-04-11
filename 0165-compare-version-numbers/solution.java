949e788d68c5256dfd98eb6e44f48d7f791fc315
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-04-11 09:58:56 +0000

    [LeetCode Sync] Runtime - 1 ms (73.12%), Memory - 41 MB (99.09%)

--- 0165-compare-version-numbers/solution.java
+++ 0165-compare-version-numbers/solution.java
@@ -1,61 +1,38 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] arr1 = version1.split("\\.");
-        String[] arr2 = version2.split("\\.");
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
 
-        int i = 0, j = 0;
+        int i = 0;
 
-        while(i < arr1.length && j < arr2.length){
-            int v1 = cleanText(arr1[i]);
-            int v2 = cleanText(arr2[j]);
-
-            // System.out.println(v1);
-            // System.out.println(v2);
-
-            if(v1 > v2){
+        while(i < v1.length && i < v2.length) {
+            int i1 = Integer.valueOf(v1[i]);
+            int i2 = Integer.valueOf(v2[i]);
+            if(i1 > i2) {
                 return 1;
             }
-            if(v1 < v2){
+            else if(i1 < i2) {
                 return -1;
             }
-
             i++;
-            j++;
         }
 
-        while(i < arr1.length){
-            if(cleanText(arr1[i]) != 0){
+        while(i < v1.length) {
+            int v = Integer.valueOf(v1[i]);
+            if(v > 0) {
                 return 1;
             }
             i++;
         }
 
-        while(j < arr2.length){
-            if(cleanText(arr2[j]) != 0){
+        while(i < v2.length) {
+            int v = Integer.valueOf(v2[i]);
+            if(v > 0) {
                 return -1;
             }
-            j++;
+            i++;
         }
 
         return 0;
-    }
-
-    private int cleanText(String s){
-        String res = "";
-        boolean flag = false;
-
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '0' && !flag){
-                continue;
-            }
-            res += s.charAt(i);
-            flag = true;
-        }
-
-        if(res.equals("")){
-            return 0;
-        }
-
-        return Integer.valueOf(res);
     }
 }
