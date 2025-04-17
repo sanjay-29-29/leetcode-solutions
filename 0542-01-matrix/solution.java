c3510efd0b758c2430fda614d6456a57707fba50
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-04-17 08:03:58 +0000

    [LeetCode Sync] Runtime - 30 ms (6.53%), Memory - 50.2 MB (5.47%)

---
+++ 0542-01-matrix/solution.java
@@ -0,0 +1,48 @@
+class Solution {
+   static int[][] dir = { { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 } };
+
+    public int[][] updateMatrix(int[][] isWater) {
+        int[][] res = new int[isWater.length][isWater[0].length];
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for (int[] i : res) {
+            Arrays.fill(i, Integer.MAX_VALUE);
+        }
+
+        for (int i = 0; i < isWater.length; i++) {
+            for (int j = 0; j < isWater[0].length; j++) {
+                if (isWater[i][j] == 0) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+
+        bfs(res, queue);
+        return res;
+    }
+
+    private void bfs(int[][] res, Queue<int[]> queue) {
+        boolean[][] visited = new boolean[res.length][res[0].length];
+        int distance = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size != 0) {
+                int[] next = queue.poll();
+                if (!(0 <= next[0] && next[0] < res.length) || !(0 <= next[1] && next[1] < res[0].length) || visited[next[0]][next[1]]) {
+                    size--;
+                    continue;
+                }
+                visited[next[0]][next[1]] = true;
+                res[next[0]][next[1]] = Math.min(
+                        res[next[0]][next[1]],
+                        distance);
+                for (int[] d : dir) {
+                    queue.add(new int[] { d[0] + next[0], d[1] + next[1] });
+                }
+                size--;
+            }
+            distance++;
+        }
+    } 
+}
