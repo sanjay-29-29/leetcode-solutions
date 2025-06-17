d5866a58d7768b5d7c2b126594428c1068ce54e3
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-06-17 17:19:00 +0000

    [LeetCode Sync] Runtime - 1 ms (97.17%), Memory - 45.7 MB (60.43%)

--- 0605-can-place-flowers/solution.java
+++ 0605-can-place-flowers/solution.java
@@ -1,41 +1,42 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
-        if(flowerbed.length == 1){
-            if(flowerbed[0] == 1){
-                if(n == 0)
-                return true;
+        if (n == 0) {
+            return true;
+        }
+
+        if (flowerbed.length == 1) {
+            if (n > 0 && flowerbed[0] != 0) {
                 return false;
             }
-            if(flowerbed[0] == 0){
-                if(n <= 1){
-                    return true;
-                }else{
-                    return false;
-                }
-            }
+            return true;
         }
-        for(int i=0; i<flowerbed.length; i++){
-            if(i==flowerbed.length-1){
-                if(flowerbed[i]==0 && flowerbed[i-1]==0){
-                    flowerbed[i] = 1;
-                    count++;
+
+        if (flowerbed.length == 2) {
+            if (n == 1 && flowerbed[0] == 0 && flowerbed[1] == 0)
+                return true;
+            return false;
+        }
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (i == 0) {
+                if (flowerbed[i] == 0 && flowerbed[1] == 0) {
+                    flowerbed[0] = 1;
+                    n--;
                 }
-                continue;
-            }
-            if(i==0){
-                if(flowerbed[i]==0 && flowerbed[i+1]==0){
+            } 
+            else if(i == flowerbed.length - 1) {
+                if(flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
                     flowerbed[i] = 1;
-                    count++;
+                    n--;
                 }
-                continue;
             }
-            else if(flowerbed[i-1] == 0 && flowerbed[i+1]==0 && flowerbed[i] != 1){
+            else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0 && flowerbed[i] == 0) {
                 flowerbed[i] = 1;
-                count++;
+                n--;
             }
         }
-        if(count>=n){
+
+        if (n <= 0) {
             return true;
         }
         return false;
