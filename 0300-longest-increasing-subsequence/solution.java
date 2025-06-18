4611e403bf32ea386efa033e406190537380d07e
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2025-06-18 17:08:26 +0000

    [LeetCode Sync] Runtime - 44 ms (43.12%), Memory - 44.2 MB (66.40%)

--- 0300-longest-increasing-subsequence/solution.java
+++ 0300-longest-increasing-subsequence/solution.java
@@ -3,20 +3,20 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
 
-        for(int i = 0; i < nums.length; i++) {
-            int max = dp[i];
-            for(int j = 0; j <= i; j++) {
-                if(nums[i] > nums[j]) {
-                    max = Math.max(max, dp[j] + 1);
+        for(int i = 0; i < nums.length; i++){
+            int curr = dp[i];
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] > nums[i]) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
                 }
             }
-            dp[i] = max;
         }
 
         int max = 1;
         for(int i : dp) {
             max = Math.max(max, i);
         }
+
         return max;
     }
 }
